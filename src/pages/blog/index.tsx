162fc999f58507d74e29c9b637be416f11785451
Sungjung Jo <toysmoon@gmail.com>
2020-01-18 00:37:47 +0900

    Hotfix: server build error

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -6,37 +6,37 @@ import PostsList from '../../components/PostsList'
 import getNotionUsers from '../../lib/notion/getNotionUsers'
 import getBlogIndex from '../../lib/notion/getBlogIndex'
 
-export async function unstable_getStaticProps() {
-  const postsTable = await getBlogIndex()
-  const authorsToGet: Set<string> = new Set()
-  const posts: any[] = Object.keys(postsTable)
-    .map(slug => {
-      const post = postsTable[slug]
-      // remove draft posts in production
-      if (!postIsReady(post)) {
-        return null
-      }
-      post.Authors = post.Authors || []
-      for (const author of post.Authors) {
-        authorsToGet.add(author)
-      }
-      return post
-    })
-    .filter(Boolean)
+// export async function unstable_getStaticProps() {
+//   const postsTable = await getBlogIndex()
+//   const authorsToGet: Set<string> = new Set()
+//   const posts: any[] = Object.keys(postsTable)
+//     .map(slug => {
+//       const post = postsTable[slug]
+//       // remove draft posts in production
+//       if (!postIsReady(post)) {
+//         return null
+//       }
+//       post.Authors = post.Authors || []
+//       for (const author of post.Authors) {
+//         authorsToGet.add(author)
+//       }
+//       return post
+//     })
+//     .filter(Boolean)
 
-  const { users } = await getNotionUsers([...authorsToGet])
+//   const { users } = await getNotionUsers([...authorsToGet])
 
-  posts.map(post => {
-    post.Authors = post.Authors.map(id => users[id].full_name)
-  })
+//   posts.map(post => {
+//     post.Authors = post.Authors.map(id => users[id].full_name)
+//   })
 
-  return {
-    props: {
-      posts,
-    },
-    revalidate: 10,
-  }
-}
+//   return {
+//     props: {
+//       posts,
+//     },
+//     revalidate: 10,
+//   }
+// }
 
 function getPostsDisplay(postsTable) {
   const authorsToGet: Set<string> = new Set()
